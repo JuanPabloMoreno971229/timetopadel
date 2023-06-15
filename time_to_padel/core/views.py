8bfa38b9848ea73cdeb0ecced243d7052fff96af
Juan Pablo Moreno <juanpablomoreno251@hotmail.com>
2023-06-14 19:02:29 -0500

    Views solved

--- time_to_padel/core/views.py
+++ time_to_padel/core/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.shortcuts import render, redirect
-from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
+from django.urls import reverse
 from .models import torneo
 from inscripcion.forms import InscripcionForm
 from django.http import HttpResponseRedirect
@@ -12,6 +12,7 @@ class TorneoListView(ListView):
 class TorneoDetailView(DetailView):
     model = torneo
     form_class = InscripcionForm
+    template_name = 'core/torneo_detail.html'  # Reemplaza 'torneo_detail.html' por tu plantilla de detalle
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -23,16 +24,12 @@ class TorneoDetailView(DetailView):
         if form.is_valid():
             torneo_id = self.kwargs['pk']
             inscripcion = form.save(commit=False)
-            inscripcion.tournament_id = torneo_id  # Asignar el valor de la clave foránea
+            inscripcion.tournament_id = torneo_id
             inscripcion.save()
             return HttpResponseRedirect(self.get_success_url())
         else:
             return self.render_to_response(self.get_context_data(form=form))
-    
+
     def get_success_url(self):
-        return reverse('torneos:torneo', args=[self.get_object().id]) + '?ok'
-    
-
-    
-        
-
+        torneo_id = self.kwargs['pk']
+        return reverse('torneos:detalle', args=[torneo_id]) + '?ok'
